0c0025b840880b31afb3453a79dd78f84ad2e9b2
RomainJ <romain.jacquesson@gmail.com>
2025-03-30 21:47:25 +0200

    Ajout class constructor exercice

--- index.js
+++ index.js
@@ -26,7 +26,44 @@ let exerciceArray = [];
 })();
 
 // Générateur d'exercice, lancer la routine minutage.
-class Exercice {}
+class Exercice {
+  constructor() {
+    this.index = 0;
+    this.minutes = exerciceArray[this.index].min;
+    this.seconds = 0;
+  }
+
+  updateCountdown() {
+    this.seconds = this.seconds < 10 ? "0" + this.seconds : this.seconds;
+
+    setTimeout(() => {
+      if (this.minutes === 0 && this.seconds === "00") {
+        this.index++;
+        if (this.index < exerciceArray.length) {
+          this.minutes = exerciceArray[this.index].min;
+          this.seconds = 0;
+          this.updateCountdown();
+        } else {
+          return page.finish();
+        }
+      } else if (this.seconds === "00") {
+        this.minutes--;
+        this.seconds = 59;
+        this.updateCountdown();
+      } else {
+        this.seconds--;
+        this.updateCountdown();
+      }
+    }, 10);
+    return (main.innerHTML = `
+            <div class ="exercice-container">
+                <p>${this.minutes}:${this.seconds}</p>
+                <img src="./img/${exerciceArray[this.index].pic}.png" />
+                <div>${this.index + 1}/${exerciceArray.length}</div>
+            </div>
+        `);
+  }
+}
 
 // Les fonctions utils du projet.
 const utils = {
@@ -123,9 +160,11 @@ const page = {
     utils.handleEventArrow();
     utils.deleteItem();
     reboot.addEventListener("click", () => utils.reboot());
+    start.addEventListener("click", () => this.routine());
   },
   routine: function () {
-    utils.pageContent("Routine", "Exercice avec chrono", null);
+    const exercice = new Exercice();
+    utils.pageContent("Routine", exercice.updateCountdown(), null);
   },
   finish: function () {
     utils.pageContent(
